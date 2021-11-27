dc441ac062b2bc6f4b437131c4d488552addfbe6
Joe <joegezo@gmail.com>
2021-11-27 12:15:17 -0500

    entirely removing adaptiveinstancenorm

--- updaug/_models.py
+++ updaug/_models.py
@@ -60,8 +60,8 @@ def build_generator(num_domains, num_channels=3):
     domain_inpt = tf.keras.layers.Input((num_domains,), dtype=tf.int64)
     net = encoder(inpt)
     #net = anorm(net, domain_inpt)
-    print("SHUNTING OUT ADAPTIVE INSTANCE NORM LAYER")
-    net = tf.keras.layers.Add()([net, 0*anorm(net, domain_inpt)])
+    #print("SHUNTING OUT ADAPTIVE INSTANCE NORM LAYER")
+    #net = tf.keras.layers.Add()([net, 0*anorm(net, domain_inpt)])
     net = decoder(net)
     return tf.keras.Model([inpt, domain_inpt], net)
 
